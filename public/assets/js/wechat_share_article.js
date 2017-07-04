d5de1a94657ae10b655da9b550348415a145f269
silentwang <silentwang@51chaohun.com>
2017-07-04 14:37:21 +0800

    mod wechat share

--- public/assets/js/wechat_share_article.js
+++ public/assets/js/wechat_share_article.js
@@ -2,7 +2,7 @@
    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
    appid: 'wx4dcfa2241b96bcb2', // 必填，公众号的唯一标识
    timestamp: $('#timestamp').html(), // 必填，生成签名的时间戳
-   noncestr: $('#noncestr').html(), // 必填，生成签名的随机串
+   nonceStr: $('#noncestr').html(), // 必填，生成签名的随机串
    signature: $('#signature').html(),// 必填，签名，见附录1
    jsapilist: ['onmenusharetimeline', 'onmenushareappmessage'] // 必填，需要使用的js接口列表，所有js接口列表见附录2
  });
